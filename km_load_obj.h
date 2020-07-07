cf88466fa4157ee72570c82d8a302abf70b92190
Jose Rico <jmrico01@gmail.com>
2020-07-07 01:18:12 -0400

    add load_obj

---
+++ km_load_obj.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "km_array.h"
+#include "km_math.h"
+#include "km_memory.h"
+#include "km_string.h"
+
+struct ObjVertex
+{
+    Vec3 pos;
+    Vec2 uv;
+};
+
+struct ObjTriangle
+{
+    ObjVertex v[3];
+};
+
+struct ObjQuad
+{
+    ObjVertex v[4];
+};
+
+struct ObjModel
+{
+    Array<ObjTriangle> triangles;
+    Array<ObjQuad> quads;
+};
+
+struct LoadObjResult
+{
+    Array<uint8> file;
+    Array<ObjModel> models;
+};
+
+bool LoadObj(const_string filePath, LoadObjResult* result, LinearAllocator* allocator);
